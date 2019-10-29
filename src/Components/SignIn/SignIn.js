d5a615a527d0d028ee38fbb03db0ddfbd47df47a
Mvahedii <vahedi.ce@gmail.com>
2019-10-29 13:00:11 +0330

    Sign-in with Email & Password

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { signInWithGoogle } from '../../Firebase/Firebase'
+import { auth, signInWithGoogle } from '../../Firebase/Firebase'
 
 import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton.js'
@@ -16,9 +16,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {
