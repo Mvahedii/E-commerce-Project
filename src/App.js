e9d898ad2622b8eb6e677a2dd78c9ffa6963cc46
Mvahedii <vahedi.ce@gmail.com>
2019-11-03 12:29:04 +0330

    Refactoring & add Conditional Rendering to Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Header from './Components/Header/Header'
@@ -44,15 +44,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
-          <Route path='/sign-in' component={SingInandSignUp} />
+          <Route
+            exact
+            path='/sign-in'
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : <SingInandSignUp />} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
