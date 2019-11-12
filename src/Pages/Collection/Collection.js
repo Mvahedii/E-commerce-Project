be9b5786154d87521e3e6e89b010b20b42cc4113
Mvahedii <vahedi.ce@gmail.com>
2019-11-12 12:14:21 +0330

    create selector for shopping routing

--- src/Pages/Collection/Collection.js
+++ src/Pages/Collection/Collection.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Collection = () => (
-    <div>
-        <h1>Collection Page</h1>
-    </div>
-)
+import { selectCollection } from '../../Redux/ShopData/ShopDataSelectors'
 
-export default Collection;
+const Collection = ({ collection }) => {
+    console.log(collection)
+    return (
+        <div>
+            <h1>Collection Page</h1>
+        </div>
+    )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(Collection);
--- src/Redux/ShopData/ShopDataSelectors.js
+++ src/Redux/ShopData/ShopDataSelectors.js
@@ -1,8 +1,21 @@
 import { createSelector } from 'reselect'
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
 const selectShopData = state => state.shopData
 
 export const selectShopDataCollection = createSelector(
     [selectShopData],
     shopData => shopData.collection
+)
+
+export const selectCollection = collectionParams => createSelector(
+    [selectShopDataCollection],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionParams])
 )
