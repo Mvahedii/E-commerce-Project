6d267c1c6b8e2970a6ea34e3b718224a25be96c6
Mvahedii <vahedi.ce@gmail.com>
2019-11-13 14:11:15 +0330

    modify selectors

--- src/Redux/ShopData/ShopDataSelectors.js
+++ src/Redux/ShopData/ShopDataSelectors.js
@@ -1,12 +1,12 @@
 import { createSelector } from 'reselect'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens: 5
+// }
 
 const selectShopData = state => state.shopData
 
@@ -20,10 +20,10 @@ export const selectCollectionForPreview = createSelector(
     collection => Object.keys(collection).map(key => collection[key])
 )
 
-export const selectCollectionTwo = collectionParams => createSelector(
-    [selectShopDataCollection],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionParams])
-)
+// export const selectCollection = collectionParams => createSelector(
+//     [selectShopDataCollection],
+//     collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionParams])
+// )
 
 export const selectCollection = collectionParams => createSelector(
     [selectShopDataCollection],
